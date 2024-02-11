e1e85ced86e769e87bab52742ea9bb9a110a21f8
Ефим <fimakrut@mail.ru>
2024-02-11 16:44:54 +0500

    Добавленный проект. Ярыгин

---
+++ Airlines_Ярыгин/Classes/TicketContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Airlines_Ярыгин.Models;
+
+namespace Airlines_Ярыгин.Classes
+{
+    public class TicketContext:Ticket
+    {
+        public static List<TicketContext> AllTicket()
+        {
+            List<TicketContext> allTickets = new List<TicketContext>();
+            return allTickets;
+        }
+    }
+}
